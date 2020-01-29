df651e758cb3f8c0612dcfddd2cbd3536336393a
Wayne Hu <wh145@duke.edu>
2020-01-29 14:06:37 -0500

    Add modal box

--- main.js
+++ main.js
@@ -1,57 +1,39 @@
+const get = document.getElementById.bind(document);
+const query = document.querySelector.bind(document);
+
 mapboxgl.accessToken = 'pk.eyJ1IjoiZW5lcmd5aW5pdGlhdGl2ZSIsImEiOiJjazV3bzUwZHgxZmwxM2pvZ2M0YWc3bWpjIn0.ZgxnCrDTlmbiljcgEuJBpA';
-
-
-// var bounds = [
-//     [-75, 40.2],
-//     [-70., 42.4]
-// ];
 
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/energyinitiative/ck4bir2rn1poi1cpne9onmxhb',
     center: [-72.670195, 41.595318],
     zoom: 8,
-    // maxBounds: bounds
     minZoom: 4
 });
-
-var modal = document.getElementById('modal');
-
-// var aboutButton = document.getElementById('about')
-
-function openModal() {
-    modal.style.display = 'block';
-}
-
-window.onclick = function (event) {
-    if (event.target == modal) {
-        this.modal.style.display = 'none';
-    }
-}
 
 // map.addControl(new mapboxgl.FullscreenControl());
 
 map.on('load', function () {
 
-    document.getElementById('west-haven').addEventListener('click', function () {
+    get('west-haven').addEventListener('click', function () {
         map.flyTo({
             center: [-72.981017, 41.260060],
             zoom: 16
         });
     });
-    document.getElementById('somers').addEventListener('click', function () {
+    get('somers').addEventListener('click', function () {
         map.flyTo({
             center: [-72.453251, 41.956257],
             zoom: 16
         });
     });
-    document.getElementById('groton').addEventListener('click', function () {
+    get('groton').addEventListener('click', function () {
         map.flyTo({
             center: [-72.068590, 41.377535],
             zoom: 15
         });
     });
-    document.getElementById('full-ct').addEventListener('click', function () {
+    get('full-ct').addEventListener('click', function () {
         map.flyTo({
             center: [-72.670195, 41.595318],
             zoom: 8
@@ -106,3 +88,23 @@ map.on('load', function () {
     });
 
 });
+
+let modal = get('modal');
+let modalContent = query('.modal-content');
+
+function openModal() {
+    modal.classList.add('visible');
+};
+
+function closeModal() {
+    modal.classList.remove('visible');
+};
+
+function isolateModalBox(e) {
+    e.stopPropagation();
+    e.stopImmediatePropagation();
+    return false;
+};
+
+modalContent.addEventListener('click', isolateModalBox);
+modal.addEventListener('click', closeModal);
